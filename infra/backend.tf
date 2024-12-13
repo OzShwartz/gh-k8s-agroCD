298a03a54e4bfbbf4c59369236b1af897b8941b9
Oz Shwartz <Ozshwartz@gmail.com>
2024-12-13 12:56:21 +0200

    upload file token and s3

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ozshwartz-technion-cicd-13578"
+    bucket         = "ozshwartz-technion-cicd-2468"
     key            = "terraform/state/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
