4cf419ff77fb4a18b4156df51244a51313c399f5
Oz Shwartz <Ozshwartz@gmail.com>
2025-01-05 11:49:13 +0200

    new versssion v4

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ozshwartz-technion-cicd-246810"
+    bucket         = "ozshwartz-technion-cicd-246810" #create before in AWS
     key            = "terraform/state/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
